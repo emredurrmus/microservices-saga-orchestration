cb50267b1636945112a1283ec7d9101f0fc39975
emred <emredurmus0@hotmail.com>
2025-07-16 22:54:01 +0300

    feat: create OrderCreatedEvent for order creation

--- orders-service/src/main/java/com/edurmus/orders/service/OrderServiceImpl.java
+++ orders-service/src/main/java/com/edurmus/orders/service/OrderServiceImpl.java
@@ -1,17 +1,26 @@
 package com.edurmus.orders.service;
 
 import com.edurmus.core.dto.Order;
+import com.edurmus.core.dto.events.OrderCreatedEvent;
 import com.edurmus.core.types.OrderStatus;
 import com.edurmus.orders.dao.jpa.entity.OrderEntity;
 import com.edurmus.orders.dao.jpa.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final String orderEventsTopic;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    public OrderServiceImpl(OrderRepository orderRepository,
+                            KafkaTemplate<String, Object> kafkaTemplate,
+                            @Value("${orders.event.topic.name}") String orderEventsTopic) {
         this.orderRepository = orderRepository;
+        this.kafkaTemplate = kafkaTemplate;
+        this.orderEventsTopic = orderEventsTopic;
     }
 
     @Override
@@ -22,6 +31,13 @@ public class OrderServiceImpl implements OrderService {
         entity.setProductQuantity(order.getProductQuantity());
         entity.setStatus(OrderStatus.CREATED);
         orderRepository.save(entity);
+
+        OrderCreatedEvent event = new OrderCreatedEvent(
+                entity.getId(),
+                entity.getCustomerId(),
+                entity.getProductId(),
+                entity.getProductQuantity());
+        kafkaTemplate.send(orderEventsTopic, event);
 
         return new Order(
                 entity.getId(),
